bd4edc2525fae208ff84696b9d68c8b61f96c45d
arncet <arnaud.cetoute@hotmail.fr>
2015-12-01 11:07:47 +0000

    install and config gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,37 @@
-var elixir = require('laravel-elixir');
+var gulp            = require('gulp');
+var less            = require('gulp-less');
+var browserify      = require('browserify');
+var notify          = require("gulp-notify");
+var watchify        = require('watchify');
+var reactify        = require('reactify');
+var source          = require('vinyl-source-stream');
+var handleErrors    = require('./gulpHandleErrors');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
-elixir(function(mix) {
-    mix.sass('app.scss');
+gulp.task('less', function () {
+    gulp.src('resources/assets/less/main.less') // path to your file
+        .pipe(less({strictMath: true}).on('error', handleErrors))
+        .pipe(gulp.dest('public/style'));
 });
+
+gulp.task('watch', function() {
+  gulp.watch('resources/assets/less/**/*.less', ['less']);
+
+  var watcher  = watchify(browserify({
+    entries: ['resources/js/app.jsx'],
+    transform: [reactify],
+    debug: true,
+    cache: {}, packageCache: {}, fullPaths: true
+  }));
+
+  return watcher.on('update', function () {
+    watcher.bundle()
+      .pipe(source('main.js'))
+      .pipe(gulp.dest('public/js'))
+      console.log('Updated');
+  })
+    .bundle()
+    .pipe(source('main.js'))
+    .pipe(gulp.dest('public/js'));
+});
+
+gulp.task('default', ['watch']);
